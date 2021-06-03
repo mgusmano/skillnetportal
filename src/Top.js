37ef7dd48db0690b49e08883ae98e4694d5f94f9
mgusmano <mgusmano@yahoo.com>
2021-06-03 07:26:36 -0400

    changes

--- src/Top.js
+++ src/Top.js
@@ -8,9 +8,9 @@ const Top = (props) => {
 
   return (
     <div style={{display:'flex',alignItems:'center',xjustifyContent:'center',height:'60px',background:'white',color:'rgb(51,124,182)',fontSize:'24px'}}>
-      <img src={SKILLNETBALL} style={{marginLeft:'80px',width:'50px'}} alt="SKILLNETBALL" />
+      {/* <img src={SKILLNETBALL} style={{marginLeft:'80px',width:'50px'}} alt="SKILLNETBALL" /> */}
       <img src={SWIPE} style={{marginLeft:'20px',width:'200px'}} alt="SWIPE" />
-      <div style={{margin:'30px 1px 1px 1px',fontSize:'11px'}}>06-02-2021(a)</div>
+      <div style={{margin:'30px 1px 1px 1px',fontSize:'11px'}}>06-03-2021(a)</div>
     </div>
   )
 }
